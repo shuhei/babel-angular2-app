8f6e1c576cf34c5dc45ed29764db24375287d46c
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-03-31 00:14:20 +0900

    Rename __define__ as __def_ to preserve source map. Thanks to @t_wada.

--- transformers/other-disable-define.js
+++ transformers/other-disable-define.js
@@ -4,7 +4,9 @@ module.exports = {
   check: t.isIdenfifier,
   Identifier: function Identifier(node, parent, scope, file) {
     if (node.name === 'define') {
-      node.name = '__define__';
+      // Advice from @t_wada
+      // https://twitter.com/t_wada/status/582560881533304833
+      node.name = '__def_';
     }
   }
 };
