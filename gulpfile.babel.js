cfc39e42feeefa7c79d2f25ad4fad26d175ba164
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-12-29 20:09:43 +0900

    Add sourcemaps.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,9 @@ import gulp from 'gulp';
 import gutil, { PluginError } from 'gulp-util';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
 
+import assign from 'object-assign';
 import browserify from 'browserify';
 import watchify from 'watchify';
 import babelify from 'babelify';
@@ -15,13 +17,13 @@ gulp.task('copy', () => {
 });
 
 gulp.task('build', ['copy'], () => {
-  const b = browserify('src/index.js')
+  const b = browserify('src/index.js', { debug: true })
     .transform(babelify);
   return bundle(b);
 });
 
 gulp.task('watch', () => {
-  const b = browserify('src/index.js', watchify.args)
+  const b = browserify('src/index.js', assign({ debug: true }, watchify.args))
     .transform(babelify);
   const w = watchify(b)
     .on('update', () => bundle(w))
@@ -42,5 +44,7 @@ function bundle(b) {
     })
     .pipe(source('bundle.js'))
     .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('public'));
 }
