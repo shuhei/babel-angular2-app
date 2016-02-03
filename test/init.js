a76ffb803f30dd00ecb07448f3de92de746817b9
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2016-02-03 11:52:59 +0900

    Upgrade angular2 to beta.2.

--- test/init.js
+++ test/init.js
@@ -2,5 +2,11 @@ import 'babel-polyfill';
 import 'reflect-metadata';
 import 'zone.js/lib/browser/zone-microtask';
 
-import {BrowserDomAdapter} from 'angular2/platform/browser'
-BrowserDomAdapter.makeCurrent();
+import {setBaseTestProviders} from 'angular2/testing';
+import {
+    TEST_BROWSER_PLATFORM_PROVIDERS,
+      TEST_BROWSER_APPLICATION_PROVIDERS
+} from 'angular2/platform/testing/browser';
+
+setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS,
+                     TEST_BROWSER_APPLICATION_PROVIDERS);
