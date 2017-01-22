6c8fee1e9b5a12c8f186d445117bb93f649c0af8
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-01-22 11:36:13 +0100

    Upgrade protractor to 5.0.0

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,13 +7,9 @@ startHttpServer();
 exports.config = {
   baseUrl: 'http://localhost:8080/',
 
-  seleniumServerJar: findSeleniumJar(),
-
   specs: [
     'test/**/*.e2e.js'
-  ],
-
-  useAllAngular2AppRoots: true
+  ]
 };
 
 
@@ -22,20 +18,4 @@ function startHttpServer() {
   server.listen(8080, function () {
     console.log('HTTP server started at http://localhost:8080');
   });
-}
-
-function findSeleniumJar() {
-  var dir = './node_modules/protractor/selenium';
-
-  var files = fs.readdirSync(dir);
-  var jar = files.filter(function (file) {
-    return /\.jar$/.test(file);
-  })[0];
-
-  if (!jar) {
-    console.error('Selenium Server JAR was not found. Run `./node_modules/.bin/webdriver-manager update`.');
-    process.exit(1);
-  }
-
-  return path.resolve(dir, jar);
 }
